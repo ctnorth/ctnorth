40db79b6ac2ada7a00be9eee017bbadeb9655196
iFarbod <ifarbod@outlook.com>
2018-01-29 17:05:55 +0330

    Move pmake.cmd to scripts/
    So src's root directory won't be cluttered.

--- scripts/pmake.cmd
+++ scripts/pmake.cmd
@@ -1,20 +1,22 @@
 :: Premake5 Invoker util
 :: Author(s):       iFarbod <ifarbod@outlook.com>
 ::
-:: Copyright (c) 2013-2018 CTNorth Team
+:: Copyright (c) 2013-2018 The CTNorth Authors
 ::
 :: Distributed under the MIT license (See accompanying file LICENSE or copy at
 :: https://opensource.org/licenses/MIT)
 
 @echo off
 
+cd ..\src
+
 if "%1" == "clean" (
     :: Invoke premake5's clean action
     echo Performing clean action...
-    buildtools\win\premake5 clean
+    premake5 clean
 ) else (
     :: Invoke premake5 with specified args and VS2017 action
-    buildtools\win\premake5 %* vs2017
+    premake5 %* vs2017
 )
 
 :: Pause for 5 seconds and auto-close the command window
